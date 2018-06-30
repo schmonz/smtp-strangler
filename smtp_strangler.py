a0ddf429f3c2d62e1c33371e73d0693d953c4e32
Amitai Schleier <schmonz-web-git@schmonz.com>
2018-06-30 09:50:14 +0200

    Catch Ctrl-C.

--- smtp_strangler.py
+++ smtp_strangler.py
@@ -14,16 +14,24 @@ def die_usage(logger):
     sys.exit(99)
 
 
+def die_interrupt(logger):
+    logger.log(b'[interrupted]\r\n')
+    sys.exit(0)
+
+
 def main(command_line_arguments):
     logger = ProtocolLogger(sys.stderr.fileno())
 
     if not command_line_arguments:
         die_usage(logger)
 
-    SMTPProtocolStrangler(
-        sys.stdin.fileno(),
-        sys.stdout.fileno(),
-    ).strangle_and_exit(logger, command_line_arguments)
+    try:
+        SMTPProtocolStrangler(
+            sys.stdin.fileno(),
+            sys.stdout.fileno(),
+        ).strangle_and_exit(logger, command_line_arguments)
+    except KeyboardInterrupt:
+        die_interrupt(logger)
 
 
 if '__main__' == __name__:

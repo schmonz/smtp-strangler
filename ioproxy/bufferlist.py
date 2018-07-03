9d6dd628c42e4b630c0bf72c8824234efcb75229
Amitai Schleier <schmonz-web-git@schmonz.com>
2018-07-03 18:47:37 +0100

    Extract AbstractBufferList.

--- ioproxy/bufferlist.py
+++ ioproxy/bufferlist.py
@@ -1,24 +1,29 @@
 import select
 
 
-class BufferList:
+class AbstractBufferList:
     def __init__(self, buffer_list):
-        self.__buffer_list = buffer_list
-        self.__fd_list = []
-        for each_buffer in self.__buffer_list:
-            self.__fd_list.append(each_buffer.read_fd)
+        self.buffer_list = buffer_list
 
     def close(self):
-        for each_buffer in self.__buffer_list:
+        for each_buffer in self.buffer_list:
             each_buffer.close()
 
     def get_all(self):
-        return self.__buffer_list
+        return self.buffer_list
+
+
+class BufferList(AbstractBufferList):
+    def __init__(self, buffer_list):
+        AbstractBufferList.__init__(self, buffer_list)
+        self.__fd_list = []
+        for each_buffer in self.buffer_list:
+            self.__fd_list.append(each_buffer.read_fd)
 
     def get_readables(self):
         (reads, writes, exceptionals) = select.select(self.__fd_list, [], [])
         readables = []
-        for each_buffer in self.__buffer_list:
+        for each_buffer in self.buffer_list:
             if each_buffer.read_fd in reads:
                 readables.append(each_buffer)
         return readables

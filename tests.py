b7a5d54071bec3cd9d0ffc15439b6e43dedfa55e
Amitai Schleier <schmonz-web-git@schmonz.com>
2018-07-04 10:05:17 +0100

    Use @unittest.skip('reason') instead of comments.

--- tests.py
+++ tests.py
@@ -9,6 +9,7 @@ from ioproxy.smtp.strangler import SMTPStringStrangler
 
 
 class TestStrangler(unittest.TestCase):
+    @unittest.skip('soon')
     def test_BRXT_is_same_as_QUIT(self):
         input = StringInput(b'BRXT plz\r\n')
         output = StringOutput()
@@ -19,22 +20,25 @@ class TestStrangler(unittest.TestCase):
 
         self.assertEqual(b'QUIT plz\r\n', output.output_string)
 
-    # def test_CONF_gives_success_code_and_url(self):
-    #     # XXX request becomes NOOP
-    #     # XXX response becomes '250 https://www.spaconference.org/spa2018/'
-    #     self.fail('not yet implemented: CONF')
-    #
-    # def test_EHLO_includes_gdpr_notice(self):
-    #     # XXX server response does not include '250 GDPR 20160414'
-    #     # XXX proxy response does
-    #     self.fail('not yet implemented: GDPR capability')
-    #
-    # def test_reject_mail_from_tim(self):
-    #     # XXX when MAIL FROM: tim,
-    #     # XXX request becomes NOOP
-    #     # XXX response becomes '553 no thank you'
-    #     self.fail('not yet implemented: rejecting mail from Tim')
-    #
+    @unittest.skip('soon')
+    def test_CONF_gives_success_code_and_url(self):
+        # XXX request becomes NOOP
+        # XXX response becomes '250 https://www.spaconference.org/spa2018/'
+        self.fail('not yet implemented: CONF')
+
+    @unittest.skip('soon')
+    def test_EHLO_includes_gdpr_notice(self):
+        # XXX server response does not include '250 GDPR 20160414'
+        # XXX proxy response does
+        self.fail('not yet implemented: GDPR capability')
+
+    @unittest.skip('soon')
+    def test_reject_mail_from_tim(self):
+        # XXX when MAIL FROM: tim,
+        # XXX request becomes NOOP
+        # XXX response becomes '553 no thank you'
+        self.fail('not yet implemented: rejecting mail from Tim')
+
     # More ideas:
     #
     # - Log `MAIL FROM` and `RCPT TO` parameters, with a timestamp
